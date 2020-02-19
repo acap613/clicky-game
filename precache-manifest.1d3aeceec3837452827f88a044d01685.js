d80c4ec2ed09d6cffb8edd95c70f60ddea22b273
Angelo Capozzi <acap613@gmail.com>
2020-02-19 18:57:50 -0500

    Updates

---
+++ precache-manifest.1d3aeceec3837452827f88a044d01685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7eabd9c2e6eeb5cbdf3439592eac6d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d27f22461141eb7cf401",
+    "url": "/clicky-game/static/css/main.ff76e6e9.chunk.css"
+  },
+  {
+    "revision": "b22ba226c8eb2fe4cca4",
+    "url": "/clicky-game/static/js/2.64cd445e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.64cd445e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27f22461141eb7cf401",
+    "url": "/clicky-game/static/js/main.82a6257b.chunk.js"
+  },
+  {
+    "revision": "14dbdf28621dc649e929",
+    "url": "/clicky-game/static/js/runtime-main.386b6ac9.js"
+  }
+]);
